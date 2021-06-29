21c101c94085fdd2d453b246ded6486a442c8b65
Nicholas Hanashiro <nicholashanashiro@gmail.com>
2021-06-28 18:02:01 -0700

    final commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,58 +1,56 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
 // YOUR CODE HERE!
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// UFO Sighting values for each column
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    // Append one table row `tr` for each UFO Sighting object
+    var row = tbody.append("tr");
+
+    // Use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
 var button = d3.select("#button");
 
-// Select the form
-var form = d3.select("#form");
+button.on("click", function() {
 
-// Create event handlers 
-button.on("click", runEnter);
-form.on("submit",runEnter);
+  tbody.html("");
 
-// Complete the event handler function for the form
-function runEnter() {
-
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-  
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#patient-form-input");
-
-  // Get the value property of the input element
+  // Select the input date get the raw HTML nodes
+  var inputElement = d3.select("#datetime");
+  // Get the value property of the input date, state, shape
   var inputValue = inputElement.property("value");
-
+  // console.log input value
   console.log(inputValue);
-  console.log(people);
-
-  var filteredData = people.filter(person => person.bloodType === inputValue);
-
+  // Filter Data with datetime equal to input value
+  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+  // console.log filter values
   console.log(filteredData);
 
-  // BONUS: Calculate summary statistics for the age field of the filtered data
 
-  // First, create an array with just the age values
-  
-  var ages = filteredData.map(person => person.age);
+  filteredData.forEach(function(selections) {
 
-  // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
-  var mean = math.mean(ages);
-  var median = math.median(ages);
-  var mode = math.mode(ages);
-  var variance = math.variance(ages);
-  var standardDeviation = math.std(ages);
-
-  // Then, select the unordered list element by class name
-  var list = d3.select(".summary");
-
-  // remove any children from the list to
-  list.html("");
-
-  // append stats to the list
-  list.append("li").text(`Mean: ${mean}`);
-  list.append("li").text(`Median: ${median}`);
-  list.append("li").text(`Mode: ${mode}`);
-  list.append("li").text(`Variance: ${variance}`);
-  list.append("li").text(`Standard Deviation: ${standardDeviation}`);
-};
+  console.log(selections);
+  // Append one table row `tr` for each UFO Sighting object
+  var row = tbody.append("tr");
+  // Use `Object.entries` to console.log each UFO Sighting value
+  Object.entries(selections).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      var cell = row.append("td");
+      cell.text(value);
+  });
+});
+});
